cf106a47d041f3b879e2af14167a325e31b39b8d
jdfermin <jesusdfermin01@gmail.com>
2023-11-30 23:09:13 -0400

    creacion base de datos inicial

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URL'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
+class Directory(db.Model)
+    __tablename__ = 'directory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def json(self):
+        return{'id': self.id, 'name': self.name}
+
+db.create_all()
+
 @app.route('/status',methods=['GET'])
 def status():
     return make_response('pong',200)
