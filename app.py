e674958a4cee06257dba98822c1afa1f4fafd07b
jdfermin <jesusdfermin01@gmail.com>
2023-12-02 17:37:34 -0400

    retornar objeto en el post

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def create_directory():
             )
         db.session.add(new_directory)
         db.session.commit()
-        return make_response(jsonify({new_directory.json()}), 201)
+        return make_response(jsonify({directory.json() for directory in new_directory}), 201)
     except e:
         return make_response(jsonify({'message': 'error al crear direcotorio'}), 500)
 
